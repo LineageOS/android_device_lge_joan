f57ca7c9ddc6445133d83b98d437984fb4600218
Han Sol Jin <hansol@hansol.ca>
2023-12-31 02:08:18 -0800

    joan: set TARGET_RECOVERY_DENSITY to xxxhdpi
    joan ships xxxhdpi in stock. We change this to something lower in the
    OS, because Android 12 and above is quite difficult to use with such a
    high density, but the recovery is also quite tiny when using 540, so
    let's use the normal density and make the recovery great again.
    
    Change-Id: I7825bff75158b11c1910e39a7ef47333766f0600

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,6 +200,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap2wake"
 
 # Recovery
 BOOTLOADER_MESSAGE_OFFSET := 128
+TARGET_RECOVERY_DENSITY := xxxhdpi
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.joan
 
 # RIL
