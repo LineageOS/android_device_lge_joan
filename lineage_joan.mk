d3243c7b8c5c1fb90c57c9f61108310ed6b40679
Han Sol Jin <hansol@hansol.ca>
2024-01-06 10:24:45 -0800

    joan: switch to SPDX-License-Identifier
    Change-Id: I327d7c20ce9fedb15557e4927699b3cbd79b1986

--- lineage_joan.mk
+++ lineage_joan.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2018-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2018-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
