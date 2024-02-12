27d25e99dea478d64965ba45afc081a332f92bf8
Han Sol Jin <hansol@hansol.ca>
2024-02-11 22:01:11 -0800

    Revert "joan: revert all DRM blobs back to stock"
    phoenix_sprout blobs are actually needed this time to work around
    missing symbol issue.
    
    This reverts commit 2bfc614258f2912ea0bdb387963c1cf9ba8c7ad9.
    
    Change-Id: I5205a2788aa822e386eb1a90b5a8f97327622c0e

--- device.mk
+++ device.mk
@@ -414,7 +414,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Recovery
 PRODUCT_COPY_FILES += \
