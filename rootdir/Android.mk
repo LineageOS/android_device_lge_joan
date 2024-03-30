43bb5055f768448050656597d5a6b4f3a0ade6c9
Ricky Cheung <rcheung844@gmail.com>
2024-03-30 13:24:55 +0800

    joan: Get rid of LGE init scripts (Part 2)
    * Standardize tab formatting
    * Merge similar files (init.msm8998.rc & init.lge.sensors.rc)
    * Remove LG factory crap
    
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>
    Change-Id: Iee82294013cb2d4c5ae66516e7c138534e9de10d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -96,22 +96,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.lge.power.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init/hw/init.lge.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.lge.sensors.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init/hw/init.lge.sensors.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.msm8998.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
