e865ac199a1e18a9e7797325a5556dfcaa2b85cc
Han Sol Jin <hansol@hansol.ca>
2024-04-26 19:41:02 -0700

    joan: migrate to common Lineage libperfmgr HAL
    Change-Id: I141ba31cff3a8bf6d541ac81cbd570f8b1a8a507

--- device.mk
+++ device.mk
@@ -364,16 +364,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.lge-libperfmgr \
-    libqti-perfd-client
+$(call inherit-product, hardware/lineage/interfaces/power-libperfmgr/power-libperfmgr.mk)
+$(call inherit-product, hardware/qcom-caf/common/libqti-perfd-client/libqti-perfd-client.mk)
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/interfaces \
-    hardware/google/pixel
 
 # QCOM
 PRODUCT_COPY_FILES += \
