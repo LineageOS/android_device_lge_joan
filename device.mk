73cecdcfe5094d2e764902ca5230bafe4a99e282
Han Sol Jin <hansol@hansol.ca>
2024-06-22 12:46:51 -0700

    joan: import NFC stack
    Change-Id: I9f76310e2c84eeb9d76c2c499ee19e487cd1114e

--- device.mk
+++ device.mk
@@ -9,6 +9,14 @@ DEVICE_PATH := device/lge/joan
 # Inherit common repository
 $(call inherit-product, device/lge/joan-common/joan-common.mk)
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.1-service
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(DEVICE_PATH)/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Releasetools
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/releasetools/device_check.sh:install/bin/device_check.sh
